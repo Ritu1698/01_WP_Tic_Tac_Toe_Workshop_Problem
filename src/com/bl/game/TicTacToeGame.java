596471b661a3f57e6e4f4c564c53125a82a5adad
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-07 11:32:17 +0530

    [Rituparna]Added: Move Validity Checking

--- src/com/bl/game/TicTacToeGame.java
+++ src/com/bl/game/TicTacToeGame.java
@@ -4,41 +4,57 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-    public static char [] makeBoard(){
-        char [] ourBoardValues = new char[10];
-        for(int boardPosition=1;boardPosition<ourBoardValues.length;boardPosition++){
+    //Function to create the board
+    public static char[] makeBoard() {
+        char[] ourBoardValues = new char[10];
+        for (int boardPosition = 1; boardPosition < ourBoardValues.length; boardPosition++) {
             ourBoardValues[boardPosition] = ' ';
         }
         return ourBoardValues;
-    }  //Function to create the board
+    }
 
-    public static char playerChoosesXor0(char userValue){
-       char playerValue = userValue;
+    //Function to assign player value
+    public static char playerChoosesXor0(char userValue) {
+        char playerValue = userValue;
         return playerValue;
-    } //Function to assign player value
+    }
 
-    public  static void  displayBoard(char [] ourBoard){
+    //Displaying Board Values
+    public static void displayBoard(char[] board) {
         System.out.println("The board currently:-");
-        System.out.println("\n"+ourBoard[1]+" | "+ourBoard[2]+ " | "+ourBoard[3]);
+        System.out.println("\n" + board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("----------");
-        System.out.println(ourBoard[4]+" | "+ourBoard[5]+ " | "+ourBoard[6]);
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("----------");
-        System.out.println(ourBoard[7]+" | "+ourBoard[8]+ " | "+ourBoard[9]);
-    } //Displaying Board Values
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
 
-    public static void main(String[] args){
+    //Check Valid Move Or Not
+    public static boolean checkUserMove(char[] board, int userPosition) {
+        if (userPosition > 0 && userPosition < board.length && board[userPosition] == ' ')
+            return true;
+        else
+            return false;
+    }
+
+    //Our main function
+    public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe Workshop Problem");
-        char [] boardValues = makeBoard();
-
+        char[] boardValues = makeBoard();
         System.out.println("Please Choose X or 0");
-        Scanner sc = new Scanner(System.in);
-        char userValue = sc.next().charAt(0);
-        sc.close();
-
+        Scanner userInput = new Scanner(System.in);
+        char userValue = userInput.next().charAt(0);
         char playerValue = playerChoosesXor0(userValue);
         char computerValue = (playerValue == 'X') ? '0' : 'X';
-        System.out.println("Value chosen by Player & Computer are :- "+playerValue+", "+computerValue);
-
+        System.out.println("Value chosen by Player & Computer are :- " + playerValue + ", " + computerValue);
+        System.out.println("Please Choose the position to make move");
+        int userPosition = userInput.nextInt();
+        boolean checkIfValidMove = checkUserMove(boardValues, userPosition);
+        userInput.close();
+        String validityResult = checkIfValidMove ? "Valid move" : "Invalid move";
+        System.out.println(validityResult);
+        boardValues[userPosition] = checkIfValidMove ? playerValue : ' ';
         displayBoard(boardValues);
-    } //Our main function
+
+    }
 }
