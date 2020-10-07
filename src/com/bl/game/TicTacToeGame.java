c33801967afcfb2b4e36b1f76380f58c748b6fa9
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-07 08:58:19 +0530

    [Rituparna]Added: Created Board

--- src/com/bl/game/TicTacToeGame.java
+++ src/com/bl/game/TicTacToeGame.java
@@ -11,7 +11,7 @@ public class TicTacToeGame {
 
         char [] ourBoardValues = new char[10];
 
-        for(int i=0;i<10;i++){
+        for(int i=1;i<10;i++){
             ourBoardValues[i] = ' ';
         }
 
