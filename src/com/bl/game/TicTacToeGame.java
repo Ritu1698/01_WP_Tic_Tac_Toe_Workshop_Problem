ab95d53836c9e713d778af135a80dd0e2e11997d
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-07 09:13:22 +0530

    [Rituparna]Added: Created Board

--- src/com/bl/game/TicTacToeGame.java
+++ src/com/bl/game/TicTacToeGame.java
@@ -3,20 +3,15 @@ package com.bl.game;
 public class TicTacToeGame {
 
     public static char [] makeBoard(){
-
         char [] ourBoardValues = new char[10];
-
-        for(int i=1;i<10;i++){
-            ourBoardValues[i] = ' ';
+        for(int boardPosition=1;boardPosition<ourBoardValues.length;boardPosition++){
+            ourBoardValues[boardPosition] = ' ';
         }
-
         return ourBoardValues;
     }
 
     public static void main (String[] args){
-
         System.out.println("Welcome to tic tac toe Workshop Problem");
         char [] boardValues = makeBoard();
-
     }
 }
